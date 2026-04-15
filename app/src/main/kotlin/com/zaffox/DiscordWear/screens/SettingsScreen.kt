9c84ac9847ee69c01bf53f49801d6d1abc46feb7
Zaffox <161511358+Zaffox@users.noreply.github.com>
2026-04-15 10:45:48 -0500

    Create SettingsScreen.kt

---
+++ app/src/main/kotlin/com/zaffox/DiscordWear/screens/SettingsScreen.kt
@@ -0,0 +1,36 @@
+package com.zaffox.discordwear.screens
+
+import android.app.RemoteInput
+import android.os.Bundle
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.wear.compose.foundation.lazy.ScalingLazyColumn
+import androidx.wear.compose.foundation.lazy.rememberScalingLazyListState
+import androidx.wear.compose.material3.*
+import androidx.wear.input.RemoteInputIntentHelper
+import androidx.wear.input.wearableExtender
+
+@Composable
+fun SettingScreen() {
+    val context   = LocalContext.current
+    val listState = rememberScalingLazyListState()
+
+    ScreenScaffold(scrollState = listState) {
+        ScalingLazyColumn(state = listState) {
+            item {
+                Text(
+                    "Settings",
+                    style     = MaterialTheme.typography.titleMedium,
+                    textAlign = TextAlign.Center,
+                    modifier  = Modifier.fillMaxWidth()
+                )
+            }
+        }
+    }
+}
